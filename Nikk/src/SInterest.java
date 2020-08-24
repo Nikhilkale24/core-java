4a4c969cbae86869a47c5a0e39484fc24056536f
Nikhilkale24 <you@example.com>
2020-08-24 15:55:06 +0530

    Assignment1

---
+++ Nikk/src/SInterest.java
@@ -0,0 +1,13 @@
+
+public class SInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double p=200,i=6,t=3;
+		double Si=p*i*t/100;
+		System.out.println("simple interest is="+Si);
+		
+
+	}
+
+}
