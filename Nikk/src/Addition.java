3755ca272c8b6b1408f22524de55caabb9629577
Nikhilkale24 <you@example.com>
2020-08-24 15:33:11 +0530

    Basic program

---
+++ Nikk/src/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=15,b=30;
+		System.out.println("First No. is="+a);
+		System.out.println("First No. is="+b); 
+		int c=a+b;
+		System.out.println("Addition of two no. is="+c);
+
+	}
+
+}
